abb81e50ac576bfe13f6400191c00ec24190967d
Fred Drake <fred.drake@gmail.com>
2021-09-27 17:14:36 -0400

    Updated vault

--- scripts/encdec.sh
+++ scripts/encdec.sh
@@ -15,7 +15,7 @@ cd ansible/
 echo "--- All files with the filename vault.yml"
 for i in $(find . -name vault.yml); do
     echo -n "   --- $i: "
-            ansible-vault $1 $i --vault-password-file .vault_pass
+            ansible-vault $1 $i
 done
 
 while read f; do
@@ -23,11 +23,11 @@ while read f; do
         echo "--- All files in directory $f"
         for i in $(find $f -type f); do
             echo -n "   --- $i: " 
-            ansible-vault $1 $i --vault-password-file .vault_pass
+            ansible-vault $1 $i
         done
     elif [[ -f "$f" ]]; then
         echo -n "--- $f: "
-        ansible-vault $1 --vault-password-file .vault_pass $f
+        ansible-vault $1 $f
     else
         echo "$f IS NOT VALID"
     fi
